8da8af6a827349bc621f99ebb2744c33fd7ae544
Jennifer Duchetta <jennifer@contovista.com>
2016-05-11 11:15:28 +0200

    additional commit

--- app/components/keywords/Keywords.js
+++ app/components/keywords/Keywords.js
@@ -4,21 +4,29 @@ import css from './Keywords.local.css';
 
 export default class Keyword extends React.Component {
   render() {
-    const list = ['a', 'b', 'c'];
-    var r = 200; //radius
+    const list = ['a', 'b', 'c', 'd', 'e'];
+    var radius = 200; //radius
     var pi = Math.PI;
     var n = list.length;
 
-    
+    var elements = [];
 
-/////(x + r cos(2kπ/n), y + r sin(2kπ/n))
+    for (var i = 0; i < n; i++) {
+      var l =500 + radius*Math.cos(2*i*pi/n);
+      var r =400 + radius*Math.sin(2*i*pi/n);
+      elements.push(<button className="btn btn-Lg btn-primary" style={{position: 'absolute', left: l, top: r}}>{list[i]}</button>);
+    }
+
+
+
+
+
 
 
     return(
       <div id="keyword-circle">
 
-        <div className="field" style={{position: 'absolute', left: '500px', top: '300px'}}>3</div>
-
+        {elements}
 
       </div>
     );
